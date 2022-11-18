53e4d9a88ecc091639e304df673282aad5425203
shinyehwan <jghisjl007@naver.com>
2022-11-18 17:45:47 +0900

    new 없이 객체 만들어주는 함수 구현, Ut.cls.newObj

--- src/main/java/com/ll/exam/Ut.java
+++ src/main/java/com/ll/exam/Ut.java
@@ -1,8 +1,26 @@
 package com.ll.exam;
 
 import java.beans.Introspector;
+import java.lang.reflect.InvocationTargetException;
 
 public class Ut {
+	public static class cls{
+
+		public static Object newObj(Class cls, Object defaultValue) {
+			try {
+				return cls.getDeclaredConstructor().newInstance();
+			} catch (InstantiationException e) {
+				throw new RuntimeException(e);
+			} catch (IllegalAccessException e) {
+				throw new RuntimeException(e);
+			} catch (InvocationTargetException e) {
+				throw new RuntimeException(e);
+			} catch (NoSuchMethodException e) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
+
 	public static class str{
 		public static String decapitalize(String string) {
 			if (string == null || string.length() == 0) {
