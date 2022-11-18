0511309d403e7159d10f98e475395c76750cda39
shinyehwan <jghisjl007@naver.com>
2022-11-18 16:50:52 +0900

    수동액션매핑과 수동객체생성을 안하기 위한 큰그림을 그린다, 어노테이션 부터 제작

---
+++ src/main/java/com/ll/exam/DispatchServlet.java
@@ -0,0 +1,31 @@
+package com.ll.exam;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/usr/*")
+public class DispatchServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp){
+
+	}
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doGet(req, resp);
+	}
+
+	@Override
+	protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doGet(req, resp);
+	}
+
+	@Override
+	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doGet(req, resp);
+	}
+}
