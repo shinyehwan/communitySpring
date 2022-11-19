54db36771582d5c40d9c0b330c70f4550c622989
shinyehwan <jghisjl007@naver.com>
2022-11-19 18:04:29 +0900

    Autowired 어노테이션 작동하도록

--- src/main/java/com/ll/exam/Container.java
+++ src/main/java/com/ll/exam/Container.java
@@ -1,12 +1,14 @@
 package com.ll.exam;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.reflections.Reflections;
 
+import com.ll.exam.annotation.Autowired;
 import com.ll.exam.annotation.Controller;
 import com.ll.exam.annotation.Service;
 
@@ -20,8 +22,40 @@ public class Container {
 	}
 
 	private static void scanComponents() {
+		// 전체 레고 생성
 		scanServices();
 		scanControllers();
+
+		// 레고 조립
+		resolveDependenciesAllComponents();
+	}
+
+	private static void resolveDependenciesAllComponents() {
+		for (Class cls : objects.keySet()) {
+			Object o = objects.get(cls);
+
+			resolveDependencies(o);
+		}
+	}
+
+	private static void resolveDependencies(Object o) {
+		Arrays.asList(o.getClass().getDeclaredFields())
+			.stream()
+			.filter(f -> f.isAnnotationPresent(Autowired.class))
+			.map(field -> {
+				field.setAccessible(true);
+				return field;
+			})
+			.forEach(field -> {
+				Class cls = field.getType();
+				Object dependency = objects.get(cls);
+
+				try {
+					field.set(o, dependency);
+				} catch (IllegalAccessException e) {
+
+				}
+			});
 	}
 
 	private static void scanServices() {
